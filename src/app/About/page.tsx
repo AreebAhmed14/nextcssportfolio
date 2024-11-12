7dd3becaaf77ee29c5b4cb89a0dd7bbe2016e997
AreebAhmed14 <areebfarooq549@gmail.com>
2024-11-12 15:54:45 -0800

    edit education

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <div>
-       <div className="aboutmain">
-           <div className="inner">
-            <h1 className="abhead">ABOUT ME</h1>
-            <p>Hello I am Areeb a 17 year old web designer and developer with a creative flair for crafting visually appealing and functional websites. Currently I am an intermediate student and a dedicated learner in the field of Gen AI at GIAIC. I have a strong passion for turning ideas into reality from building responsive designs to developing efficient code. With hands on experience in Next.js and Tailwind CSS I create seamless and modern websites optimized for user experience. I am also constantly exploring new technologies like TypeScript and Node.js to expand my skill set
+      <div className="aboutmain">
+        <div className="inner aboutcover">
+          <h1 className="abhead">ABOUT ME</h1>
+          <center>
+          <p>I am Areeb a web designer and developer with a passion for creating functional and visually engaging digital experiences. I enjoy working on diverse projects. like Areebixbeat, a platform for sharing music, and Areebixweather, a weather-checking site.  My focus is on blending creativity with technology, and I am always looking to learn and grow in web development and design.
+    </p>
+    </center>
 
-I thrive in collaborative environments where I can share ideas and work on innovative projects
-My goal is to stay at the forefront of web development trends and continuously enhance my expertise
-Let’s collaborate and create something amazing together bringing impactful and user focused solutions to life
-            </p>
-           </div>
+    <center>
+        <p className="secondaboutpara">
+        Currently, I am diving deeper into new technologies like TypeScript and Next.js to expand my skill set. I also have experience with tools like Node.js and am working on projects. like a CV generator and a YouTube channel about Pakistans updates and facts. My goal is to keep evolving in the tech space, staying ahead of trends, and creating impactful solutions.</p>
+      </center>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/Education/page.tsx
+++ src/app/Education/page.tsx
@@ -3,10 +3,23 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className="aboutmain">
+      <div className="educationmain">
+         {/* <h1 className="abhead">EDUCATION</h1> */}
         <div className="inner">
-         <h1 className="abhead">EDUCATION</h1>
-         <p>Hello! I am Areeb a 17-year-old web designer and developer with a creative flair for crafting visually appealing and functional websites. I completed my matriculation in 2023 with a 75% score and am currently in my second year of intermediate studies at Aisha Bawany College. In the same year I completed a Computer Information Technology (CIT) course certified by SDC Pakistan and I am now expanding my knowledge in cloud technologies as a Cloud Applied student at GIAIC. My academic journey reflects my passion for technology and my continuous drive to enhance my skills. I have hands on experience with Next.js and Tailwind CSS creating responsive user-friendly websites and am continuously exploring new technologies like TypeScript and Node.js to expand my skill set. Lets collaborate and bring innovative ideas to life.</p>
+         <h1 className='educationsubheading'>MATRICULATION</h1>
+         <p>I completed my matriculation in 2023 with a score of 75%. This was a key phase in my education where I developed a foundation in core subjects such as maths and chemistry which prepared me for advanced studies.</p>
+        </div>
+        <div className="inner">
+         <h1 className='educationsubheading'>INTERMEDIATE</h1>
+         <p>Currently I am pursuing my second year of intermediate studies. During this phase I am focusing on deepening my understanding of subjects like physics chemistry and maths while continuing to explore my passion for technology and computer science.</p>
+        </div>
+        <div className="inner">
+         <h1 className='educationsubheading'>CIT</h1>
+         <p>In parallel with my academic studies I have also obtained a certification in Information Technology (CIT). This certification has equipped me with practical skills in software applications enhancing my ability to work in the digital and tech world.</p>
+        </div>
+        <div className="inner">
+         <h1 className='educationsubheading'>GEN AI</h1>
+         <p>As part of my ongoing professional development I am also a student of Generative AI under the Governor Sindh IT Initiative. This program has provided me a understanding of AI technologies particularly in creating generative models and exploring applications in various fields</p>
         </div>
      </div>
     </div>
